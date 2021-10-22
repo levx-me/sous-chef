42e1dc7022e5fbf60ecd32db037d7f1c183c4114
TheGreatHB <kavanaboy@gmail.com>
2021-10-22 18:24:01 +0900

    Add test contracts

---
+++ contracts/test/TestLPToken.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.8;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+
+contract TestLPToken is ERC20("TestLPToken", "TESTLP") {
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external {
+        _burn(from, amount);
+    }
+}
