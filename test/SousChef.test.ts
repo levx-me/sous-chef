90bc95afc96d58536cf12d033fbcb39096f75c49
TheGreatHB <kavanaboy@gmail.com>
2021-11-17 21:48:33 +0900

    Update SousChef.test.ts

--- test/SousChef.test.ts
+++ test/SousChef.test.ts
@@ -1,11 +1,11 @@
-import { SousChef, SushiYieldToken, TestMasterChef, TestSushiBar, TestLPToken, TestSushiToken } from "../typechain";
+import { SousChef, SushiYieldToken, TestMasterChef, TestSushiBar, TestLPToken, TestSushiToken, RewardBucket, RewardFountain, TestRewardToken } from "../typechain";
 
 import { ethers } from "hardhat";
-import { expect, assert } from "chai";
+import { expect } from "chai";
 import { BigNumber, BigNumberish, BytesLike } from "ethers";
 import { getBlock, mine, mineTo, autoMining } from "./utils/blockchain";
 
-const { constants } = ethers;
+const { constants, utils } = ethers;
 const { AddressZero, HashZero } = constants;
 
 const rpb = 10000;
@@ -56,9 +56,128 @@ const setupTest = async () => {
     };
 };
 
+async function getYieldToken(sChef: SousChef, pid: number): Promise<SushiYieldToken> {
+    const address = await sChef.getYieldTokenAddress(pid);
+    const SushiYieldTokenFactory = await ethers.getContractFactory("SushiYieldToken");
+    return (await SushiYieldTokenFactory.attach(address)) as SushiYieldToken;
+}
+
 describe("SousChef", () => {
     beforeEach(async () => {
         await ethers.provider.send("hardhat_reset", []);
+    });
+
+    it("should be that getYieldTokenAddress function works corretly", async () => {
+        const { sChef, masterChef } = await setupTest();
+
+        const TestLPToken = await ethers.getContractFactory("TestLPToken");
+
+        const lp1 = (await TestLPToken.deploy()) as TestLPToken;
+        const lp2 = (await TestLPToken.deploy()) as TestLPToken;
+        const lp3 = (await TestLPToken.deploy()) as TestLPToken;
+
+        await masterChef.add(1, lp1.address, false);
+        await masterChef.add(2, lp2.address, false);
+        await masterChef.add(3, lp3.address, false);
+
+        const y0 = (await getYieldToken(sChef, 0)).address;
+        const y1 = (await getYieldToken(sChef, 1)).address;
+        const y2 = (await getYieldToken(sChef, 2)).address;
+        const y3 = (await getYieldToken(sChef, 3)).address;
+
+        await expect(sChef.createYieldTokens([2], [AddressZero]))
+            .to.emit(sChef, "YieldTokenCreated")
+            .withArgs(2, lp2.address, y2, AddressZero);
+
+        await expect(sChef.createYieldTokens([1], [AddressZero]))
+            .to.emit(sChef, "YieldTokenCreated")
+            .withArgs(1, lp1.address, y1, AddressZero);
+
+        expect((await sChef.yTokenInfoOf(0)).yieldToken).to.be.equal(AddressZero);
+        expect((await sChef.yTokenInfoOf(3)).yieldToken).to.be.equal(AddressZero);
+        expect((await sChef.yTokenInfoOf(1)).yieldToken).to.be.equal(y1);
+        expect((await sChef.yTokenInfoOf(2)).yieldToken).to.be.equal(y2);
+
+        await sChef.createYieldTokens([3, 0], [AddressZero, AddressZero]);
+        expect((await sChef.yTokenInfoOf(3)).yieldToken).to.be.equal(y3);
+        expect((await sChef.yTokenInfoOf(0)).yieldToken).to.be.equal(y0);
+    });
+
+    it("should be that createYieldTokens function works properly", async () => {
+        const { alice, sChef, masterChef, lp } = await setupTest();
+
+        const TestLPToken = await ethers.getContractFactory("TestLPToken");
+
+        const lp1 = (await TestLPToken.deploy()) as TestLPToken;
+        const lp2 = (await TestLPToken.deploy()) as TestLPToken;
+        const lp3 = (await TestLPToken.deploy()) as TestLPToken;
+
+        await masterChef.add(1, lp1.address, false);
+        await masterChef.add(2, lp2.address, false);
+        await masterChef.add(3, lp3.address, false);
+
+        await expect(sChef.connect(alice).createYieldTokens([0, 1], [AddressZero, AddressZero])).to.be.revertedWith(
+            "Ownable: caller is not the owner"
+        );
+        await expect(sChef.createYieldTokens([0, 4], [AddressZero, AddressZero])).to.be.reverted;
+
+        await sChef.createYieldTokens([0, 1], [AddressZero, AddressZero]);
+
+        await expect(sChef.createYieldTokens([0], [AddressZero])).to.be.revertedWith("SOUSCHEF: YIELD_TOKEN_EXISTS");
+        await expect(sChef.createYieldTokens([0, 1], [AddressZero, AddressZero])).to.be.revertedWith(
+            "SOUSCHEF: YIELD_TOKEN_EXISTS"
+        );
+
+        const yt0 = await getYieldToken(sChef, 0);
+        const yt1 = await getYieldToken(sChef, 1);
+
+        expect(await yt0.pid()).to.be.equal(0);
+        expect(await yt1.pid()).to.be.equal(1);
+
+        expect(await yt0.lpToken()).to.be.equal(lp.address);
+        expect(await yt1.lpToken()).to.be.equal(lp1.address);
+        expect(await yt0.lpToken()).to.be.equal((await masterChef.poolInfo(0)).lpToken);
+        expect(await yt1.lpToken()).to.be.equal((await masterChef.poolInfo(1)).lpToken);
+
+        await masterChef.add(4, lp.address, false);
+        expect((await masterChef.poolInfo(0)).lpToken).to.be.equal((await masterChef.poolInfo(4)).lpToken);
+
+        await sChef.createYieldTokens([4], [AddressZero]);
+        const yt4 = await getYieldToken(sChef, 4);
+        expect(await yt0.lpToken()).to.be.equal(await yt4.lpToken());
+    });
+
+    it("should be that starategy can be updated", async () => {
+        const { alice, sChef, masterChef, lp } = await setupTest();
+
+        const TestLPToken = await ethers.getContractFactory("TestLPToken");
+
+        const lp1 = (await TestLPToken.deploy()) as TestLPToken;
+
+        await masterChef.add(1, lp1.address, false);
+
+        const TestRewardToken = await ethers.getContractFactory("TestRewardToken");
+        const rewardToken0 = (await TestRewardToken.deploy()) as TestRewardToken;
+        const rewardToken1 = (await TestRewardToken.deploy()) as TestRewardToken;
+
+        const RewardBucket = await ethers.getContractFactory("RewardBucket");
+        const rBucket = (await RewardBucket.deploy(sChef.address, [rewardToken0.address], [10])) as RewardBucket;
+    
+        const RewardFountain = await ethers.getContractFactory("RewardFountain");
+        const rFountain = (await RewardFountain.deploy(sChef.address, [rewardToken0.address, rewardToken1.address], [1, 2])) as RewardFountain;
+
+        await sChef.createYieldTokens([0, 1], [AddressZero, rBucket.address]);
+
+        expect((await sChef.yTokenInfoOf(0)).strategy).to.be.equal(AddressZero);
+        expect((await sChef.yTokenInfoOf(1)).strategy).to.be.equal(rBucket.address);
+
+        await expect(sChef.updateStrategy(2, AddressZero)).to.be.revertedWith("SOUSCHEF: YIELD_TOKEN_NOT_EXISTS");
+        await expect(sChef.connect(alice).updateStrategy(0, AddressZero)).to.be.revertedWith("Ownable: caller is not the owner");
+
+        await sChef.updateStrategy(0, rFountain.address);
+        await sChef.updateStrategy(1, rFountain.address);
+        expect((await sChef.yTokenInfoOf(0)).strategy).to.be.equal(rFountain.address);
+        expect((await sChef.yTokenInfoOf(1)).strategy).to.be.equal(rFountain.address);
     });
 
     it("overall test-1", async () => {
